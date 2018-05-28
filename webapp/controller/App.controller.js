82e5a24f2e491894dbdb632378a53b5e019722a2
francocaprabuild <franco.capra.build@gmail.com>
2018-05-28 01:40:59 +0000

    Init repository

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+
+], function(Controller, MessageToast){
+	"use strict";
+							  //name space + folder + artifact name
+	return Controller.extend( "opensap.myapp.controller.App" , {	
+		onShowHello : function(){
+			MessageToast.show("SAPUI5 is ready");
+		}
+	});
+});
