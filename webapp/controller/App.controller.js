105c20cc5dde355cc0430daf530b12413146e96a
francocaprabuild <franco.capra.build@gmail.com>
2018-05-28 02:52:23 +0000

    CREATING A CONFIGURABLE APP COMPONENT

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,12 @@
 sap.ui.define([
-
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast"
-
-], function(Controller, MessageToast){
+], function (Controller, MessageToast) {
 	"use strict";
 							  //name space + folder + artifact name
-	return Controller.extend( "opensap.myapp.controller.App" , {	
-		onShowHello : function(){
+	return Controller.extend("opensap.myapp.controller.App", {	
+		
+		onShowHello : function () {
 			MessageToast.show("SAPUI5 is ready");
 		}
 	});
