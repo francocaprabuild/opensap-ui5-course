4a24afaa612f198e5cc443eb14f889650ac29ff3
francocaprabuild <franco.capra.build@gmail.com>
2018-06-07 12:27:37 +0000

    week2 unit 6

--- webapp/Component.js
+++ webapp/Component.js
@@ -12,9 +12,9 @@ sap.ui.define([
 		init : function () {
 			// call the init function of the parent
 			UIComponent.prototype.init.apply(this, arguments);
-
+			
 			// additional initialization can be done here
+			this.getModel().setUseBatch(false);
 		}
-
 	});
 });
